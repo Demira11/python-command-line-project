c4ff8ea36870eae4d8fecbc64036606ceec3f241
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-12-01 16:43:24 -0500

    added a function to the main.py to print the users input

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from peewee import SqliteDatabase, Model, CharField
+from peewee import SqliteDatabase, Model, CharField, IntegerField
 import argparse
 
 # Import the 'review' function from flashcards module
@@ -10,8 +10,8 @@ class Flashcard(Model):
     title = CharField()
     question = CharField()
     answer = CharField()
-    correct_count = CharField(default=0)
-    incorrect_count = CharField(default=0)
+    correct_count = IntegerField(default=0)
+    incorrect_count = IntegerField(default=0)
 
     class Meta:
         database = db
@@ -19,35 +19,33 @@ class Flashcard(Model):
 db.connect()
 db.create_tables([Flashcard])
 
-def create_flashcard(title, question, answer):
-    Flashcard.create(title=title, question=question, answer=answer)
-
-def list_flashcards():
-    return Flashcard.select()
-
 def main():
     parser = argparse.ArgumentParser(description="Flashcards CLI")
     parser.add_argument('--create', action='store_true', help='Create a new flashcard')
     parser.add_argument('--train', type=int, help='Start a training session with the specified number of cards')
     parser.add_argument('--list', action='store_true', help='List all flashcards')
     parser.add_argument('--review', type=int, help='Review flashcards with the specified number')
-    parser.add_argument('--stats', action='store_true', help='Show flashcards statistics')
 
     args = parser.parse_args()
 
     if args.create:
         # Code for creating a flashcard (similar to what you already have)
-        pass
+        title = input("Enter the title: ")
+        question = input("Enter the question: ")
+        answer = input("Enter the answer: ")
+        create_flashcard(title, question, answer)
+        print("Flashcard created!")
     elif args.train:
-        train(args.train)
+        results = train(args.train)
+        print("Training session results:")
+        print(f"Correct Answers: {results['correct']} / Incorrect Answers: {results['incorrect']}")
     elif args.list:
-        # Code for listing flashcards (similar to what you already have)
-        pass
+        flashcards = list_flashcards()
+        print("List of flashcards:")
+        for flashcard in flashcards:
+            print(f"Title: {flashcard.title}, Question: {flashcard.question}, Answer: {flashcard.answer}")
     elif args.review:
         review(args.review)
-    elif args.stats:
-        # Code for displaying flashcards statistics
-        pass
 
 if __name__ == "__main__":
     main()
