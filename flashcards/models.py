06bd513cc626ab622965aeb17a82c5820181db4f
Demira11 <117664846+Demira11@users.noreply.github.com>
2023-12-01 14:07:28 -0500

    created the main, modle and seed files in the flashcards directory and added models.py code

---
+++ flashcards/models.py
@@ -0,0 +1,16 @@
+
+from peewee import SqliteDatabase, Model, CharField, IntegerField
+
+db = SqliteDatabase('flashcards.db')
+
+class FlashCard(Model):
+    front = CharField()
+    back = CharField()
+    correct_answers = IntegerField(default=0)
+    incorrect_answers = IntegerField(default=0)
+
+    class Meta:
+        database = db
+
+db.connect()
+db.create_tables([FlashCard])
